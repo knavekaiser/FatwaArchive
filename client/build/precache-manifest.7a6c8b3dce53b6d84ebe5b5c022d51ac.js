a10cfdbdc1741cfb70a45ce7994053f56a72d913
knavekaiser <naeem.ahmad.9m@gmail.com>
2020-11-21 02:08:37 +0600

    build is included

---
+++ client/build/precache-manifest.7a6c8b3dce53b6d84ebe5b5c022d51ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bcb8b7cdbe7b4f6d5c207169a3c2022",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a20edb32ad0705ce209",
+    "url": "/static/css/main.b2563597.chunk.css"
+  },
+  {
+    "revision": "777bf28ffbb5824fd3fa",
+    "url": "/static/js/2.d5ff0a11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d5ff0a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a20edb32ad0705ce209",
+    "url": "/static/js/main.8d691e6e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "abf045e52d9afed74eb61647ad25cb68",
+    "url": "/static/media/logo.abf045e5.svg"
+  }
+]);
